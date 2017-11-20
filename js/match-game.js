4ef1ce6bebc5a2eb1e741d12b62383cf86d29d90
Bree <bdantuna@gmail.com>
2017-11-19 21:28:13 -0600

    add card value function

--- js/match-game.js
+++ js/match-game.js
@@ -10,7 +10,24 @@ var MatchGame = {};
  */
 
 MatchGame.generateCardValues = function () {
-
+ 
+    var unplacedCards = [];
+    
+    for (var value=1; value <= 8; value++) {
+        unplacedCards.push(value);
+        unplacedCards.push(value);
+    }
+    
+    var randomlyPlacedCards = [];
+    
+    while (unplacedCards.length > 0) {
+        var randomIndex = Math.floor(Math.random() * unplacedCards.length);
+        var placed = unplacedCards.splice(randomIndex, 1)[0];
+        randomlyPlacedCards.push(placed);
+        
+    }
+    
+    return randomlyPlacedCards;
 };
 
 /*
@@ -19,8 +36,20 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
-
-};
+    var bgImg = [
+        url(../img/choochoo.jpg),
+         url(../img/darla.jpg),
+         url(../img/chama.jpg),
+         url(../img/grace.jpg),
+         url(../img/hodor.jpg),
+         url(../img/iggy.jpg),
+         url(../img/neuce.jpg),
+         url(../img/ripley.jpg)
+    ];
+    
+    $game.empty();
+    
+}
 
 /*
   Flips over a given card and checks to see if two cards are flipped over.
